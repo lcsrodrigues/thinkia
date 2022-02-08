f98657fc52ee87c80be3cdf1cf7d355d6340f837
Lucas Rosa Rodrigues <lcsrodrigues@hotmail.com>
2022-02-08 09:42:32 -0300

    alteração no nº de telefone

--- components/section05/index.tsx
+++ components/section05/index.tsx
@@ -13,7 +13,7 @@ export default function Section05() {
                 <p>Nossos departamentos estão a sua disposição para questões comerciais assim como novos projetos e suporte técnico. Utilize um dos nossos canais abaixo e teremos prazer em atendê-lo! </p>
             </div>
             <div className={style.boxCard}>
-                <CardModel03 title={"FALE CONOSCO"} description={"(021) 3078-2222"} pathImg={"/phone-call.png"} />
+                <CardModel03 title={"FALE CONOSCO"} description={"(021) 3589-9055"} pathImg={"/phone-call.png"} />
                 <CardModel03 title={"SUPORTE"} description={"suporte@thinkia.com.br"} pathImg={"/checkbox.png"} />
                 <CardModel03 title={"ENDEREÇO"} description={"Rua Maria Mendes Vechi 270, Sala 7, Centro, Mesquita RJ - 26553-070"} pathImg={"/tool.png"} />
                 <CardModel03 title={"EMAIL"} description={"comercial@thinkia.com.br"} pathImg={"/email.png"} />
