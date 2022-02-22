8150beb2f78041a2e9478c84b0a69748a0716357
Lucas Rosa Rodrigues <lcsrodrigues@hotmail.com>
2022-02-21 22:08:47 -0300

    criaçaõ de um carrousel

--- components/section06/index.tsx
+++ components/section06/index.tsx
@@ -1,13 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { CardModel04 } from '../card/card';
 import style from './style.module.scss';
 import arrayTestemunho from './testemunhos.json';
 
 export default function Section06() {
 
-    useEffect(() => {
+    const [data, setData] = useState([]);
+    const carousel = useRef(null);
 
-    })
+    const handleLeftClick = (e) => {
+        e.preventDefault();
+        carousel.current.scrollLeft -= carousel.current.offsetWidth;
+    };
+
+    const handleRightClick = (e) => {
+        e.preventDefault();
+
+        carousel.current.scrollLeft += carousel.current.offsetWidth;
+    };
 
     return (
         <div id="testemunhos" className={style.section}>
@@ -18,12 +28,22 @@ export default function Section06() {
                 <p>Leia de quem confiou em nossos serviços.</p>
             </div>
             <div className={style.boxCard}>
-                {
-                    arrayTestemunho &&
-                    arrayTestemunho.map((testemunho, index) => {
-                        return <CardModel04 key={index} office={testemunho.office} signature={testemunho.signature} title={testemunho.title} description={testemunho.description} pathImg={testemunho.pathImg} />
-                    })
-                }
+                <div className={style.carousel} ref={carousel}>
+                    {
+                        arrayTestemunho &&
+                        arrayTestemunho.map((testemunho, index) => {
+                            return <CardModel04 key={index} office={testemunho.office} signature={testemunho.signature} title={testemunho.title} description={testemunho.description} pathImg={testemunho.pathImg} />
+                        })
+                    }
+                </div>
+                <div className={style.buttons}>
+                    <button onClick={handleLeftClick}>
+                        <img src="./arrow.png" alt="Scroll Left" />
+                    </button>
+                    <button onClick={handleRightClick}>
+                        <img src="./arrow.png" alt="Scroll Right" />
+                    </button>
+                </div>
             </div>
         </div>
     )
